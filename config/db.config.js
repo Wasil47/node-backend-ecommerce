b1fc41f9011613e085f76871da65fd27a0b939bf
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-07-10 18:20:18 +0200

    test

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
 /* Set connect with SQL Database */
 const db = mysql.createConnection({
-  host: process.env.SQL_SERVER, // "localhost"
-  user: process.env.SQL_LOGIN, // "root"
-  password: process.env.SQL_PASSWORD, // 1234
-  database: process.env.SQL_DB_NAME, // "shop_react_sql"
+  // host: process.env.SQL_SERVER, // "localhost"
+  // user: process.env.SQL_LOGIN, // "root"
+  // password: process.env.SQL_PASSWORD, // 1234
+  // database: process.env.SQL_DB_NAME, // "shop_react_sql"
 });
 /* If connection throw an error, try this commend in ur database:
 ALTER USER '<user>'@'localhost' IDENTIFIED WITH mysql_native_password BY '<password>' */
