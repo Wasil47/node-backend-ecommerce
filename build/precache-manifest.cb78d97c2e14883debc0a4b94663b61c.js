3e72e050d0c7096501f9c7753c3a8ff9f71d0883
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-07-11 09:25:08 +0200

    deploy heroku

---
+++ build/precache-manifest.cb78d97c2e14883debc0a4b94663b61c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990d1e9ecc03b9d763e97e9d301c538a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1ea6d7c18b1ff615496",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1963cd858c79c6a8ffd",
+    "url": "./static/css/main.50390a66.chunk.css"
+  },
+  {
+    "revision": "a1ea6d7c18b1ff615496",
+    "url": "./static/js/2.e9ff46bc.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "./static/js/2.e9ff46bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1963cd858c79c6a8ffd",
+    "url": "./static/js/main.0cbb71a7.chunk.js"
+  },
+  {
+    "revision": "62dd55573bc3d27d0d18",
+    "url": "./static/js/runtime-main.8121c213.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4560de661f44019842efb53f40a337ab",
+    "url": "./static/media/testProduct.4560de66.jpg"
+  }
+]);
